f038a3b8b2f931baeb48f79c8c2703ba60c453a8
Mark Fine <mark@seville.hitronhub.home>
2023-08-08 16:04:22 -0400

    Changes.

--- home.nix
+++ home.nix
@@ -23,43 +23,8 @@
     wget
 
     # Dev stuff
-    # (agda.withPackages (p: [ p.standard-library ]))
-    google-cloud-sdk
-    haskellPackages.cabal-install
-    haskellPackages.hoogle
-    haskellPackages.hpack
-    haskellPackages.implicit-hie
-    haskellPackages.stack
-    idris2
     jq
-    nodePackages.typescript
-    nodejs
-    purescript
-
-    # Useful nix related tools
-    cachix # adding/managing alternative binary caches hosted by Cachix
-    # comma # run software from without installing it
-    niv # easy dependency management for nix projects
-    nodePackages.node2nix
-
   ] ++ lib.optionals stdenv.isDarwin [
-    cocoapods
     m-cli # useful macOS CLI commands
   ];
-
-  # Misc configuration files --------------------------------------------------------------------{{{
-
-  # https://docs.haskellstack.org/en/stable/yaml_configuration/#non-project-specific-config
-  home.file.".stack/config.yaml".text = lib.generators.toYAML {} {
-    templates = {
-      scm-init = "git";
-      params = {
-        author-name = "Your Name"; # config.programs.git.userName;
-        author-email = "youremail@example.com"; # config.programs.git.userEmail;
-        github-username = "yourusername";
-      };
-    };
-    nix.enable = true;
-  };
-
 }
