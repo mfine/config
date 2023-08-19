aba90d56a0976d1faa734526f2aaa502efac8abe
Mark Fine <mark.fine@gmail.com>
2023-08-19 14:44:05 -0700

    working

--- configuration.nix
+++ configuration.nix
@@ -1,30 +1,9 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
-  # Nix configuration ------------------------------------------------------------------------------
-
-  nix.binaryCaches = [
-    "https://cache.nixos.org/"
-  ];
-  nix.binaryCachePublicKeys = [
-    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-  ];
-  nix.trustedUsers = [
-    "@admin"
-  ];
-  users.nix.configureBuildUsers = true;
-
-  # Enable experimental nix command and flakes
-  # nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    auto-optimise-store = true
-    experimental-features = nix-command flakes
-  '' + lib.optionalString (pkgs.system == "aarch64-darwin") ''
-    extra-platforms = x86_64-darwin aarch64-darwin
-  '';
-
-  # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;
-
-  # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
+  programs.zsh.enable = true;
+  users.users.mark.home = "/Users/mark";
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  home-manager.users.mark = import ./home.nix;
 }
--- flake.nix
+++ flake.nix
@@ -2,60 +2,20 @@
   description = "Mark's darwin system";
 
   inputs = {
-    # Package sets
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
-    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-
-    # Environment/system management
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+      home-manager.url = "github:nix-community/home-manager/release-23.05";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      darwin.url = "github:lnl7/nix-darwin";
+      darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
-  let 
-
-    inherit (darwin.lib) darwinSystem;
-    inherit (inputs.nixpkgs-unstable.lib) attrValues makeOverridable optionalAttrs singleton;
-
-    # Configuration for `nixpkgs`
-    nixpkgsConfig = {
-      config = { allowUnfree = true; };
-    }; 
-  in
-  {
-    # My `nix-darwin` configs
-    darwinConfigurations = rec {
-      mark-one = darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-          # Main `nix-darwin` config
-          ./configuration.nix
-          # `home-manager` module
-          home-manager.darwinModules.home-manager
-          {
-            nixpkgs = nixpkgsConfig;
-            # `home-manager` config
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mark = import ./home.nix;
-          }
-        ];
-      };
+  outputs = { self, nixpkgs, home-manager, darwin }: {
+    darwinConfigurations.seville = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+        home-manager.darwinModules.home-manager
+        ./configuration.nix
+      ];
     };
-
-    # Overlays --------------------------------------------------------------- {{{
-
-    overlays = {
-      # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          # Add access to x86 packages system is running Apple Silicon
-          pkgs-x86 = import inputs.nixpkgs-unstable {
-            system = "x86_64-darwin";
-            inherit (nixpkgsConfig) config;
-          };
-        }; 
-      };
- };
+  };
 }
