d151c9cdc8d7587a0571386286cd816c5a3678d9
Mark Fine <mark.fine@gmail.com>
2023-08-21 10:44:30 -0700

    Minimal install.

--- configuration.nix
+++ configuration.nix
@@ -2,10 +2,10 @@
 {
   services.nix-daemon.enable = true;
   programs.zsh.enable = true;
-  users.users.mark.home = "/Users/mark";
+  users.users.mark.home = "/Users/mark"; # mark!
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
-  home-manager.users.mark = import ./home.nix;
+  home-manager.users.mark = import ./home.nix; # mark!
   homebrew = {
     enable = true;
     onActivation.autoUpdate = true;
--- home.nix
+++ home.nix
@@ -2,19 +2,6 @@
 {
   home.stateVersion = "23.05";
   home.packages = [
-    pkgs.jq
-    pkgs.htop
-    pkgs.ledger
-    pkgs.direnv
-    pkgs.nix-direnv
-    pkgs.silver-searcher
-    pkgs.go_1_20
-    pkgs.golangci-lint
-    pkgs.gopls
     pkgs.tree
   ];
-  home.file.".screenrc".source = ./files/screenrc;
-  home.file.".spacemacs".source = ./files/spacemacs;
-  home.file.".gitconfig".source = ./files/gitconfig;
-  home.file.".zshrc".source = ./files/zshrc;
 }
